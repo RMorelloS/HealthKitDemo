b12b274347526b01456a00f527f798100a4c4070
Ricardo Morello Santos <ricardo_morello@hotmail.com>
2019-11-02 11:39:48 -0600

    Initial Commit

---
+++ HealthKitDemo3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthKitDemo3
+//
+//  Created by Morello Santos Ricardo on 11/2/19.
+//  Copyright © 2019 Morello Santos Ricardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
